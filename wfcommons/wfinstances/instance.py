f2c7823b3afc7da133132153d98bcb7031c9e2a5
Alexander Groth <sheypex@gmail.com>
2023-06-14 13:18:23 +0200

    fixing WfInstance.json parsing in chef

--- wfcommons/wfinstances/instance.py
+++ wfcommons/wfinstances/instance.py
@@ -88,7 +88,7 @@ class Instance:
         # Global properties
         self.executed_at: datetime = dateutil.parser.parse(
             self.instance['workflow']['executedAt'])
-        self.makespan: int = self.instance['workflow']['makespanInSeconds']
+        self.makespan: int = self.instance['workflow']['makespan']
 
         # Machines
         if 'machines' in self.instance['workflow'].keys():
