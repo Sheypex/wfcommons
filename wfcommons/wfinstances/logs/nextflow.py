ed1aa717ce9c428415a8af592a2851261c3e12c4
Alexander Groth <sheypex@gmail.com>
2023-06-21 14:44:11 +0200

    get me out

--- wfcommons/wfinstances/logs/nextflow.py
+++ wfcommons/wfinstances/logs/nextflow.py
@@ -181,7 +181,7 @@ def _parse_task_name(task_name: str):
     :return: Formatted task name
     :rtype: str
     """
-    return re.fullmatch(r"(.*?)( \(.*?\))?",task_name).group(1)
+    return task_name  # re.fullmatch(r"(.*?)( \(.*?\))?",task_name).group(1)
 
 
 def _parse_number(number: str):
